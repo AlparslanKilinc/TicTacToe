028a9d1f92305f920db271d23a4f0318695ed545
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-12-21 00:41:37 -0500

    win / tie logic + mobile css

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+const  Player = (name,value,turn) =>{
+    let pattern = [];
+
+    const getName  = ()=> name;
+    const getValue = ()=> value;
+    const getTurn = ()=> turn;
+    const getPattern= ()=>pattern;
+    const setTurn  = (playerTurn)=> turn=playerTurn;
+    const setName  = (playerName) => name=playerName;
+    const addMove  = (position) => pattern.push(position);
+    const clearPattern = ()=>pattern=[];
+
+    return {name,value,turn,pattern,getTurn,getName,getValue,setTurn,setName,addMove,getPattern,clearPattern}
+ }
+
+ let player1 = Player('player1','X',true);
+ let player2 = Player('player2','O',false);
+
 /// Board Module 
- const Board = ( ()=> {
+ const Board = ( (player1,player2)=> {
     /// Variables
     let board=document.querySelector('#board');
     let restart=document.querySelector('#restart');
+    let restart_main=document.querySelector('#restart-main');
+    let resultScreen= document.querySelector('#result-screen');
+    let resultText= document.querySelector('#result-text');
     let pattern=[];
+
     /// Create Board & Button
     restart.addEventListener('click',()=>{clearBoard()});
+    restart_main.addEventListener('click',()=>{clearBoard()});
     board.style.gridTemplateColumns= `repeat(${3}, 1fr)`;
     board.style.gridTemplateRows= `repeat(${3}, 1fr)`;
     let index=0;
@@ -24,13 +47,22 @@
         let name2=document.querySelector('#player-2');
         name1.value="";
         name2.value="";
-
+        resultScreen.style.display='none';
+        pattern=[];
+        player1.clearPattern();
+        player2.clearPattern();
     }
     const addToBoard = (player,div,index) =>{
-        if(pattern.includes(index)) return false;
+        if(pattern.includes(index)){
+            return false;
+        }
         else{
             pattern.push(index);
             div.innerHTML=player.getValue();
+            if(pattern.length>=9){
+                resultScreen.style.display='flex';
+                resultText.innerHTML='It is a Tie';
+            }
             return true;
         }
     }
@@ -41,27 +73,13 @@
         board:board,
     }
 
- })();
+ })(player1,player2);
 
- const  Player = (name,value,turn) =>{
-    let pattern = [];
-
-    const getName  = ()=> name;
-    const getValue = ()=> value;
-    const getTurn = ()=> turn;
-    const setTurn  = (playerTurn)=> turn=playerTurn;
-    const setName  = (playerName) => name=playerName;
-    const addMove  = (position) => pattern.push(position);
-
-    return {name,value,turn,pattern,getTurn,getName,getValue,setTurn,setName,addMove}
- }
-
- let player1 = Player('player1','X',true);
- let player2 = Player('player2','O',false);
 
  const GamePlay = ((Board,player1,player2) =>{
     let player;
     let turn=document.querySelector('#turn');
+    
 
     let UpdateNames =()=>{
 
@@ -98,9 +116,26 @@
     let AddMoves = ()=>{
         let board = Board.board;
         let boxes=board.querySelectorAll('div');
+        let resultScreen= document.querySelector('#result-screen');
+        let resultText= document.querySelector('#result-text');
         boxes.forEach((div,index) => div.addEventListener('click',()=>{
             let change= Board.addToBoard(player,div,index)
-            if(change)changeTurn();
+            if(change){
+                player.addMove(index);
+                // Winning
+                if(player.getPattern().includes(0) && player.getPattern().includes(1)  && player.getPattern().includes(2) ||
+                   player.getPattern().includes(3) && player.getPattern().includes(4)  && player.getPattern().includes(5) ||
+                   player.getPattern().includes(6) && player.getPattern().includes(7)  && player.getPattern().includes(8) ||
+                   player.getPattern().includes(0) && player.getPattern().includes(3)  && player.getPattern().includes(6) ||
+                   player.getPattern().includes(1) && player.getPattern().includes(4)  && player.getPattern().includes(7) ||
+                   player.getPattern().includes(2) && player.getPattern().includes(5)  && player.getPattern().includes(8) ||
+                   player.getPattern().includes(0) && player.getPattern().includes(4)  && player.getPattern().includes(8) ||
+                   player.getPattern().includes(2) && player.getPattern().includes(4)  && player.getPattern().includes(6)) {
+                        resultScreen.style.display='flex';
+                        resultText.innerHTML=`${player.getName()} Won`;
+                    }
+                changeTurn();
+            }
         }));
     }
 
